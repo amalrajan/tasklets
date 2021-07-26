a758f222df11bc43ce3d38db096b407b2aa957d7
Amal Rajan <amalrajan74@gmail.com>
2021-07-26 10:53:34 +0530

    Simple tasklet with parameters

--- src/main/java/com/example/taskletsbasic/TaskletsBasicApplication.java
+++ src/main/java/com/example/taskletsbasic/TaskletsBasicApplication.java
@@ -1,13 +1,39 @@
 package com.example.taskletsbasic;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
-public class TaskletsBasicApplication {
+public class TaskletsBasicApplication implements CommandLineRunner {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
 
     public static void main(String[] args) {
         SpringApplication.run(TaskletsBasicApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("count", "3")
+                .addString("name", "Cherry")
+                .toJobParameters();
+
+        JobExecution execution = jobLauncher.run(job, jobParameters);
+        System.out.println("STATUS :: " + execution.getStatus());
     }
 
 }
--- src/main/java/com/example/taskletsbasic/TaskletsConfig.java
+++ src/main/java/com/example/taskletsbasic/TaskletsConfig.java
@@ -5,9 +5,26 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 @EnableBatchProcessing
@@ -15,15 +32,37 @@ public class TaskletsConfig {
 
     @Autowired private JobBuilderFactory jobs;
     @Autowired private StepBuilderFactory steps;
+    @Autowired private DataSource dataSource;
+    @Autowired private JobRepository jobRepository;
+
+//    @Bean
+//    public LinesReader linesReader() {
+//        return new LinesReader();
+//    }
+
+//    @Bean
+//    public LinesProcessor linesProcessor() {
+//        return new LinesProcessor();
+//    }
 
     @Bean
-    public LinesReader linesReader() {
-        return new LinesReader();
+    @StepScope
+    public Tasklet linesReader() {
+        return (stepContribution, chunkContext) -> {
+            return RepeatStatus.FINISHED;
+        };
     }
 
     @Bean
-    public LinesProcessor linesProcessor() {
-        return new LinesProcessor();
+    @StepScope
+    public Tasklet linesProcessor(@Value("#{jobParameters['count']}") String count,
+                                    @Value("#{jobParameters['name']}") String name) {
+        return (stepContribution, chunkContext) -> {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            jdbcTemplate.update("insert into amal.users(count, name) VALUES(?, ?)", count, name);
+
+            return RepeatStatus.FINISHED;
+        };
     }
 
 
@@ -39,10 +78,9 @@ public class TaskletsConfig {
     protected Step processLines() {
         return steps
                 .get("processLines")
-                .tasklet(linesProcessor())
+                .tasklet(linesProcessor(null, null))
                 .build();
     }
-
 
     @Bean
     public Job job() {
@@ -52,4 +90,5 @@ public class TaskletsConfig {
                 .next(processLines())
                 .build();
     }
+
 }
